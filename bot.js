82238e87e6781ba5d4f561fd61bae839031dfc30
Jupiter <jupiterbaudot@gmail.com>
2018-04-02 15:57:46 -0700

    Add more names

--- bot.js
+++ bot.js
@@ -1,14 +1,14 @@
-// var Twit = require(‘twit’);
-// var TwitterBot = require(‘node-twitterbot’).TwitterBot;
-//
-//
-//
-// var Bot = new TwitterBot({
-//     consumer_key: process.env.BOT_CONSUMER_KEY,
-//     consumer_secret: process.env.BOT_CONSUMER_SECRET,
-//     access_token: process.env.BOT_ACCESS_TOKEN,
-//     access_token_secret: process.env.BOT_ACCESS_TOKEN_SECRET
-// });
+var Twit = require(‘twit’);
+var TwitterBot = require(‘node-twitterbot’).TwitterBot;
+
+
+
+var Bot = new TwitterBot({
+    consumer_key: process.env.BOT_CONSUMER_KEY,
+    consumer_secret: process.env.BOT_CONSUMER_SECRET,
+    access_token: process.env.BOT_ACCESS_TOKEN,
+    access_token_secret: process.env.BOT_ACCESS_TOKEN_SECRET
+});
 
 
 var firstPhraseArray = [
@@ -40,7 +40,13 @@ var firstPhraseArray = [
     "__",
     "//",
     "Electric",
-    "Giga"
+    "Giga",
+    "Nostalgic",
+    "Euphoric",
+    "サー",
+    "でし",
+    "はした",
+    " の コ ン "
 ]
 
 
@@ -50,18 +56,22 @@ var secondPhraseArray = [ "Sublimated",
     "84",
     "Digital",
     "V|H|S",
+    "H O N G K O N G",
+    "Ecstasy",
     "Beta",
     "Mall",
     "Disk",
     "Drive",
     "Nexus",
     "lonely",
+    "Crystal",
     "nobody",
     "a n y w h e r e",
     "n o w h e r e",
     "m e m o r y",
     "m e m o r i e s",
     "8-Bit",
+    "d e p r e s s i o n",
     "Byte",
     "Windows",
     "Future",
@@ -111,7 +121,10 @@ var secondPhraseArray = [ "Sublimated",
     "Moonlight",
     "Sleep",
     "A Q U A",
-    "Future"
+    "Future",
+    "A U D I O",
+    "C A P I T A L I S T",
+    "T O K Y O"
 
 
 ];
@@ -140,7 +153,12 @@ var thirdPhraseArray = [
     ".com",
     "-scape",
     "Nights",
-    "Futures"
+    "Futures",
+    "でした",
+    "最高で",
+    "95",
+    "84",
+    "98"
 
 ]
 
@@ -177,15 +195,13 @@ function chooseRandom(firstPhraseArray, secondPhraseArray, thirdPhraseArray) {
     }else {
         // console.log(`{first} {second} {third}`)
         vaporName = first + " " + second + " " + third
-        // console.log(first + " " + second + " " + third)
-        console.log(vaporName)
-        // return `{first} {second} {third}`
+        console.log(vaporName) // return `{first} {second} {third}`
         return vaporName
     }
-
-
 }
 
 
 
-chooseRandom(firstPhraseArray, secondPhraseArray, thirdPhraseArray)
+var name = chooseRandom(firstPhraseArray, secondPhraseArray, thirdPhraseArray)
+
+Bot.tweet(name);
